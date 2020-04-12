66dc8f09db28dedfecd4e289fc3fd66fa857bdc5
ronjones340 <ej07696@gmail.com>
2020-04-12 22:47:31 +0300

    *beta7*

--- src/periodtype.py
+++ src/periodtype.py
@@ -11,10 +11,12 @@
   #"population": 66622705,
   #"totalHospitalBeds": 1380614
 #}
-#
-#
+
+
+
+
 def days(data):
-        availablebeds = data["totalHospitalBeds"] * 0.45
+        availablebeds = data["totalHospitalBeds"] * 0.35
         impact= {
               'currentlyInfected': data['reportedCases']* 10,
                 }
@@ -31,8 +33,8 @@ def days(data):
         severeImpact['casesForICUByRequestedTime'] = int(0.05 * severeImpact['infectionsByRequestedTime'])
         impact['casesForVentilatorsByRequestedTime'] = int(0.02 * impact['infectionsByRequestedTime'])
         severeImpact['casesForVentilatorsByRequestedTime'] = int(0.02 * severeImpact['infectionsByRequestedTime'])
-        impact['dollarsInFlight'] = int(impact['infectionsByRequestedTime'] * 0.65 * 1.5 * data['timeToElapse'])
-        severeImpact['dollarsInFlight'] = int(severeImpact['infectionsByRequestedTime'] * 0.65 * 1.5 * data['timeToElapse'])
+        impact['dollarsInFlight'] = int(impact['infectionsByRequestedTime'] * 0.65 * 1.5 / data['timeToElapse'])
+        severeImpact['dollarsInFlight'] = int(severeImpact['infectionsByRequestedTime'] * 0.65 * 1.5 / data['timeToElapse'])
         result = {
           'data': data,
           'impact': impact,
@@ -41,7 +43,7 @@ def days(data):
         return result
 
 def weeks(data):
-        availablebeds = data["totalHospitalBeds"] * 0.45
+        availablebeds = data["totalHospitalBeds"] *0.35
         impact = {
               'currentlyInfected': data['reportedCases']* 10,
                 }
@@ -68,7 +70,7 @@ def weeks(data):
         return result
 
 def months(data):
-        availablebeds = data["totalHospitalBeds"] * 0.45
+        availablebeds = data["totalHospitalBeds"] * 0.35
         impact = {
               'currentlyInfected': data['reportedCases']* 10,
                 }
@@ -79,18 +81,17 @@ def months(data):
         severeImpact['infectionsByRequestedTime'] = severeImpact['currentlyInfected'] * pow(2, int(data['timeToElapse']*30 / 3))
         impact['severeCasesByRequestedTime'] = int(0.15 * impact['infectionsByRequestedTime'])
         severeImpact['severeCasesByRequestedTime'] = int(0.15 * severeImpact['infectionsByRequestedTime'])
-        impact['hospitalBedsByRequestedTime'] = int(availablebeds - impact['severeCasesByRequestedTime'])
-        severeImpact['hospitalBedsByRequestedTime'] = int(availablebeds - severeImpact['severeCasesByRequestedTime'])
+        impact['hospitalBedsByRequestedTime'] = availablebeds - impact['severeCasesByRequestedTime']
+        severeImpact['hospitalBedsByRequestedTime'] = availablebeds - severeImpact['severeCasesByRequestedTime']
         impact['casesForICUByRequestedTime'] = int(0.05 * impact['infectionsByRequestedTime'])
         severeImpact['casesForICUByRequestedTime'] = int(0.05 * severeImpact['infectionsByRequestedTime'])
         impact['casesForVentilatorsByRequestedTime'] = int(0.02 * impact['infectionsByRequestedTime'])
         severeImpact['casesForVentilatorsByRequestedTime'] = int(0.02 * severeImpact['infectionsByRequestedTime'])
         impact['dollarsInFlight'] = format(impact['infectionsByRequestedTime'] * 0.65 * 1.5 * data['timeToElapse'])
-        severeImpact['dollarsInFlight'] = severeImpact['infectionsByRequestedTime'] * 0.65 * 1.5 * data['timeToElapse'] * 30)
+        severeImpact['dollarsInFlight'] = format(severeImpact['infectionsByRequestedTime'] * 0.65 * 1.5 * data['timeToElapse'] * 30)
         result = {
           "data": data,
           "impact": impact,
           "severeImpact" : severeImpact
         }
         return result
-
